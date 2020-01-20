2c2f023f7ca8fe781c22071f8e4d8d777e8309ad
Tim Schmidt <schmidt.tim97@web.de>
2020-01-20 09:31:08 +0100

    Replaced customers and employees ArrayList by users ArrayList in UserManager

--- src/main/java/de/altwagen/user/UserManager.java
+++ src/main/java/de/altwagen/user/UserManager.java
@@ -9,13 +9,11 @@ public class UserManager {
 
     private static UserManager instance;
 
-    private ArrayList<Customer> customers;
-    private ArrayList<Employee> employees;
+    private ArrayList<User> users;
 
     private UserManager()
     {
-        customers = new ArrayList<>();
-        employees = new ArrayList<>();
+        users = new ArrayList<>();
     }
 
     public static UserManager getInstance()
@@ -44,17 +42,11 @@ public class UserManager {
      * @return new registered customer or null if a customer with that email already exists
      */
     public Customer registerCustomer(String firstname, String lastname, String eMail, String password, String country, String city, String postcode, String street, String houseNumber) {
-        for (Customer customer : customers) {
-            if (customer.getEMail().equals(eMail)) {
-                return null;
-            }
-        }
-
         Address address = new Address(country, city, postcode, street, houseNumber);
         Customer newCustomer = new Customer(firstname, lastname, eMail, password, address);
-        customers.add(newCustomer);
 
-        return newCustomer;
+        User user = registerUser(newCustomer, false);
+        return  user != null ? (Customer) user : null;
     }
 
     /**
@@ -71,78 +63,56 @@ public class UserManager {
      * @return new registered employee or null if a employee with that email already exists
      */
     public Employee registerEmployee(String firstname, String lastname, String eMail, String password, String country, String city, String postcode, String street, String houseNumber) {
-        for (Employee employee : employees) {
-            if (employee.getEMail().equals(eMail)) {
+        Address address = new Address(country, city, postcode, street, houseNumber);
+        Employee newEmployee = new Employee(firstname, lastname, eMail, password, address);
+
+        User user = registerUser(newEmployee, false);
+        return  user != null ? (Employee) user : null;
+    }
+
+    /**
+     * remove user with the specific email address
+     * @param email
+     * @return true, if user was found, false otherwise
+     */
+    public boolean deleteUser(String email){
+        boolean success = false;
+        for (User user : users) {
+            if (user.getEMail().equals(email)) {
+                users.remove(user);
+                success = true;
+                break;
+            }
+        }
+        return success;
+    }
+
+    /**
+     * search in the List for a user with the given email
+     * @param email
+     * @return user with given email, null if no user is found
+     */
+    public User getUserByEMail(String email){
+        for (User user : users) {
+            if (user.getEMail().equals(email)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // private functions
+
+    private User registerUser(User newUser, boolean isEmployee){
+        for (User user : users) {
+            if (user.getEMail().equals(newUser.getEMail())) {
                 return null;
             }
         }
-
-        Address address = new Address(country, city, postcode, street, houseNumber);
-        Employee newEmployee = new Employee(firstname, lastname, eMail, password, address);
-        employees.add(newEmployee);
-
-        return newEmployee;
+        users.add(newUser);
+        return  newUser;
     }
 
-    /**
-     * remove employee with the specific email address
-     * @param email
-     * @return true, if employee was found, false otherwise
-     */
-    public boolean deleteEmployee(String email){
-        boolean success = false;
-        for (Employee employee : employees) {
-            if (employee.getEMail().equals(email)) {
-                employees.remove(employee);
-                success = true;
-                break;
-            }
-        }
-        return success;
-    }
 
-    /**
-     * remove customer with the specific email address
-     * @param email
-     * @return true, if customer was found, false otherwise
-     */
-    public boolean deleteCustomer(String email){
-        boolean success = false;
-        for (Customer customer : customers) {
-            if (customer.getEMail().equals(email)) {
-                customers.remove(customer);
-                success = true;
-                break;
-            }
-        }
-        return success;
-    }
 
-    /**
-     * search in the List for a customer with the given email
-     * @param email
-     * @return customer with given email, null if no customer is found
-     */
-    public Customer getCustomerByEMail(String email){
-        for (Customer customer : customers) {
-            if (customer.getEMail().equals(email)) {
-                return customer;
-            }
-        }
-        return null;
-    }
-
-    /**
-     * search in the List for a employee with the given email
-     * @param email
-     * @return employee with given email, null if no employee is found
-     */
-    public Employee getEmployeeByEMail(String email){
-        for (Employee employee : employees) {
-            if (employee.getEMail().equals(email)) {
-                return employee;
-            }
-        }
-        return null;
-    }
 }
--- src/test/java/de/altwagen/user/UserManagerTest.java
+++ src/test/java/de/altwagen/user/UserManagerTest.java
@@ -3,6 +3,9 @@ package de.altwagen.user;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserManagerTest {
@@ -22,55 +25,40 @@ public class UserManagerTest {
     }
 
     @Test
-    void customerShouldBeDeleted() {
-
-        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
-
-        // Prerequisite: customer has to be registered before deleting him
-        assertNotNull(customer, "prerequisite failed: customer should be registered!");
-
-        // test
-        assertTrue(userManager.deleteCustomer(customer.eMail), "customer should be deleted!");
-    }
-
-    @Test
     void employeeShouldBeRegistered() {
         Employee employee = userManager.registerEmployee("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
         assertNotNull(employee, "employee should be registered!");
     }
 
     @Test
-    void employeeShouldBeDeleted() {
-        Employee employee = userManager.registerEmployee("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+    void usersShouldBeDeleted() {
 
-        // Prerequisite: employee has to be registered before deleting him
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Employee employee = userManager.registerEmployee("Max", "Mustermann", "mm2@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+        // Prerequisite: users has to be registered before deleting him
+        assertNotNull(customer, "prerequisite failed: customer should be registered!");
         assertNotNull(employee, "prerequisite failed: employee should be registered!");
 
         // test
-        assertTrue(userManager.deleteEmployee(employee.eMail), "employee should be deleted!");
+        assertTrue(userManager.deleteUser(customer.eMail), "customer should be deleted!");
+        assertTrue(userManager.deleteUser(employee.eMail), "customer should be deleted!");
     }
 
     @Test
-    void customerShouldBeFoundByAGivenEmail(){
+    void usersShouldBeFoundByEmail(){
         Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Employee employee = userManager.registerEmployee("Max", "Mustermann", "mm2@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
 
         // Prerequisite: customer has to be registered
         assertNotNull(customer, "prerequisite failed: customer should be registered!");
-
-        // test
-        Customer newCustomer = userManager.getCustomerByEMail(customer.eMail);
-        assertEquals(newCustomer, customer, "newCustomer and customer should be identical!");
-    }
-
-    @Test
-    void employeeShouldBeFoundByAGivenEmail(){
-        Employee employee = userManager.registerEmployee("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
-
-        // Prerequisite: employee has to be registered
         assertNotNull(employee, "prerequisite failed: employee should be registered!");
 
-        // test
-        Employee newEmployee = userManager.getEmployeeByEMail(employee.eMail);
+    // test
+        Customer newCustomer = (Customer) userManager.getUserByEMail(customer.eMail);
+        assertEquals(newCustomer, customer, "newCustomer and customer should be identical!");
+        Employee newEmployee = (Employee) userManager.getUserByEMail(employee.eMail);
         assertEquals(newEmployee, employee, "newEmployee and employee should be identical!");
     }
+
 }
