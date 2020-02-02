1ea6a4e33f8174a21972a17dc67dc3c28a4a62d5
Firyfly <44208184+Firyfly@users.noreply.github.com>
2020-02-02 20:04:44 +0100

    Add files via upload

--- src/test/java/de/altwagen/user/CustomerTest.java
+++ src/test/java/de/altwagen/user/CustomerTest.java
@@ -1,25 +1,92 @@
 package de.altwagen.user;
 
+import de.altwagen.Car.Car;
+import de.altwagen.Car.CarManager;
+import de.altwagen.Request.Request;
+import de.altwagen.Request.RequestManager;
+import de.altwagen.domain.LocationManager;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
+import static de.altwagen.Car.CarStatus.NOT_READY_FOR_SALE;
+import static de.altwagen.Request.RequestStatus.CANCELLED;
+import static de.altwagen.Request.RequestType.BUY;
+import static de.altwagen.Request.RequestType.SELL;
 import static org.junit.jupiter.api.Assertions.*;
-
 class CustomerTest {
 
+
+    private UserManager userManager;
+    private LocationManager locationManager;
+    private RequestManager requestManager;
+    private CarManager carManager;
+
+    @BeforeEach
+    void beforeEach(){
+
+        UserManager.clearInstance();
+        userManager = UserManager.getInstance();
+
+        LocationManager.clearInstance();
+        locationManager = LocationManager.getInstance();
+
+        RequestManager.clearInstance();
+        requestManager = RequestManager.getInstance();
+
+        CarManager.clearInstance();
+        carManager = CarManager.getInstance();
+    }
+
+
+
     @Test
-    void requestSellCar() {
+    void shouldCreateRequestSellCar() {
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+        Request request = customer.requestSellCar(car,5000);
+
+        assertNotNull(request, "request should be created");
+        assertEquals(request.getType(),SELL,"request type should be SELL");
 
     }
 
     @Test
-    void requestBuyCar() {
+    void shouldCreateRequestBuyCar() {
+
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+        Request request = customer.requestBuyCar(car,5000);
+
+        assertNotNull(request, "request should be created");
+        assertEquals(request.getType(),BUY,"request type should be BUY");
+
     }
 
     @Test
     void cancelRequest() {
+
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+        Request request = customer.requestBuyCar(car,5000);
+        customer.cancelRequest(request);
+
+        assertEquals(request.getStatus(),CANCELLED,"Request status should be CANCELLED");
+
     }
 
     @Test
     void decreaseRequestCount() {
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+        Request request = customer.requestBuyCar(car,5000);
+        customer.decreaseRequestCount();
+        assertEquals(customer.getActiveRequestCount(),0,"Active request count should be 0");
+
     }
+
 }
--- src/test/java/de/altwagen/user/EmployeeTest.java
+++ src/test/java/de/altwagen/user/EmployeeTest.java
@@ -1,24 +1,130 @@
 package de.altwagen.user;
 
-import org.junit.jupiter.api.Test;
 
+import de.altwagen.Car.Car;
+import de.altwagen.Car.CarManager;
+import de.altwagen.Exceptions.CarCountBelowZeroException;
+import de.altwagen.Request.Request;
+import de.altwagen.Request.RequestManager;
+import de.altwagen.Request.RequestStatus;
+import de.altwagen.domain.Location;
+import de.altwagen.domain.LocationManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
+import static de.altwagen.Car.CarStatus.FOR_SALE;
+import static de.altwagen.Car.CarStatus.NOT_READY_FOR_SALE;
+import static de.altwagen.Request.RequestStatus.*;
+import static de.altwagen.Request.RequestType.BUY;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    private UserManager userManager;
+    private LocationManager locationManager;
+    private RequestManager requestManager;
+    private CarManager carManager;
+
+    @BeforeEach
+    void beforeEach(){
+
+        UserManager.clearInstance();
+        userManager = UserManager.getInstance();
+
+        LocationManager.clearInstance();
+        locationManager = LocationManager.getInstance();
+
+        RequestManager.clearInstance();
+        requestManager = RequestManager.getInstance();
+
+        CarManager.clearInstance();
+        carManager = CarManager.getInstance();
+    }
+
+
     @Test
-    void sellCar() {
+    void requestShouldBeAccepted() {
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Request request = requestManager.addRequest(BUY,car,customer,5000);
+        request.setStatus(PENDING);
+
+        Employee employee = userManager.registerEmployee("Maxi", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+
+        assertEquals(employee.acceptRequest(request),true,"Request Status should be accepted");
+
+    }
+
+
+
+    @Test
+    void locationShouldBeChanged() throws CarCountBelowZeroException {
+        Employee employee = userManager.registerEmployee("Maxi", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Location location = locationManager.addLocation("england","London", "99086", "LeuneburgUfer", "2","Test",5, 2);
+
+        employee.changeCarLocation(car,location);
+        assertEquals(car.getLocation(), location,"car location should be new location");
+
     }
 
     @Test
-    void acceptRequest() {
+    void shouldGetLocationswithEnoughSpace() {
+        Employee employee = userManager.registerEmployee("Maxi", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Location location = locationManager.addLocation("england","London", "99086", "LeuneburgUfer", "2","Test",5, 2);
+
+        ArrayList<Location> locationss;
+        locationss = new ArrayList<>();
+        locationss.add(location);
+
+
+        assertEquals(employee.getLocationsWithAvailableSpace(1),locationss,"The one location should be put out");
+
     }
 
     @Test
-    void cancelRequest() {
+    void shouldGetPendingRequests() {
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Request request = requestManager.addRequest(BUY,car,customer,5000);
+        request.setStatus(RequestStatus.PENDING);
+
+        Employee employee = userManager.registerEmployee("Maxi", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+
+        ArrayList<Request> requestss;
+        requestss = new ArrayList<>();
+        requestss.add(request);
+
+        assertEquals(requestManager.getRequestsByStatus(RequestStatus.PENDING),requestss,"nd");
+        assertEquals(employee.listPendingRequests(),requestss,"should put out the pending request");
+
     }
 
     @Test
-    void listPendingRequests() {
+    void requestShouldBeDenied() {
+        Customer customer = userManager.registerCustomer("Max", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+        Request request = requestManager.addRequest(BUY,car,customer,5000);
+
+        Employee employee = userManager.registerEmployee("Maxi", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        employee.denyRequest(request);
+
+        assertEquals(request.getStatus(),1,"Request status should be denied");
+
+    }
+
+    @Test
+    void carShouldBeForSale() {
+       Car car = carManager.addNewCar("z1a2b3c4d","2000", 123, "Tesla", "Testmodel","Note",locationManager.addLocation("germany","Erfurt", "99086", "LeuneburgUfer", "2","Test",5, 2),NOT_READY_FOR_SALE);
+
+        Employee employee = userManager.registerEmployee("Maxi", "Mustermann", "mm@fh-erfurt.de", "1234a", "Deutschland", "Erfurt", "99087", "Altonaer Straße", "1");
+        employee.sellCar(car,5000);
+
+        assertEquals(car.getStatus(),FOR_SALE,"Request should be accepted");
+
     }
 }
