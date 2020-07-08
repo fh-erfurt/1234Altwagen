462e5e8a120cba32e504c5faf9bd70e87e88d1db
Theo Heußner <56825518+theoheussner@users.noreply.github.com>
2020-07-08 16:14:53 +0200

    Controller Test

---
+++ src/test/java/de/altwagen/Controller/GreetingApplicationTest.java
@@ -0,0 +1,13 @@
+package de.altwagen.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
